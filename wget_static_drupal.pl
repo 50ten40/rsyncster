d165242cfeb52bedd7dc32893a186c538e2ea091
Kelley Graham <kelley@50ten40.com>
2018-10-24 19:51:14 -0700

    Update and rename 2b_wget_static_drupal.pl to wget_static_drupal.pl

--- wget_static_drupal.pl
+++ wget_static_drupal.pl
@@ -4,5 +4,5 @@
 die("Please provide one or more URLs.\n") unless ($ARGV[0]);
 my $waitTime = 1;
 $waitTime = $ARGV[1] if ($ARGV[1]);
-chdir('/var/www/html/static_sites') or die "$!";
+chdir('/var/www/html/staging') or die "$!";
 system("wget -mpk --base=$ARGV[0] --user-agent=\"\" --restrict-file-names=windows -e robots=off --wait $waitTime https://m.$ARGV[0]");
