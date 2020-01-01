da8ff6d9edbcc2ff57fe225ddf7ee4938a32c42b
50ten40 <kelley@50ten40.com>
2019-12-31 18:41:34 -0700

    december changes to wget

--- drupalfiles_get.sh
+++ drupalfiles_get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ignore_files=(all default kelleygraham.com)
+ignore_files=(all default kelleygraham.com permatecture.pro faf.photos braingurus.com signup.fafchat.com signup.faf.chat signup.faf.photos rodpweiss.com signup.faf.social signup.mastery.chat)
 
 cd /var/www/html/kelleygraham.com/sites
 shopt -s dotglob 
