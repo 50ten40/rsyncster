46ba9f594cc55c2fa05ef22df42a324c6910dedd
Kelley Graham <kelley@durastudio.com>
2020-05-20 10:04:54 -0700

    freebsd port. log and comment updates for switch to time keyword

--- lib/function_sync.sh
+++ lib/function_sync.sh
@@ -20,12 +20,12 @@ sync() {
 
       echo " - TASK : Sync of $i" started >> $status
       
-      #START_TIME=`echo $(($(date +%s%N/1000000000)))`
-      $RSYNCSTER_SCRIPT $i upgrade #cron individual pages listicle feature broke for now.
+      #START_TIME=`echo $(($(date +%s%N/1000000000)))` # not portable for default *nix shell, using time keyword when calling sync().
+      $RSYNCSTER_SCRIPT $i upgrade # passing upgrade command. future default pulls only changed items using listicle feature, eg only get what's changed.
       #END_TIME=`echo $(($(date +%s%N/1000000000)))`
       #ELAPSED_TIME=$(($END_TIME - $START_TIME))
       
-      #echo "$(timestamp) - FUNCTION : Sync of $i completed in $ELAPSED_TIME seconds" >> $status
+      echo "$(timestamp) - FUNCTION : Sync of $i completed. Execution stats displayed in console" >> $status 
       
       /bin/rmdir /tmp/$CHANGES_STRING.sync.$i.lock
       echo "$(timestamp) - SUCCESS : Deleted $CHANGES_STRING.sync.$i.lock" >> $status
