12292dcab9f675de7cbec2e47082571f5ea0e6e8
50ten40 <kelley@50ten40.com>
2020-05-27 14:58:52 -0700

    updated end_of_year_updates.sh

--- end_of_year_site_updates.sh
+++ end_of_year_site_updates.sh
@@ -6,15 +6,23 @@ LIBPATH="$HOME/rsyncster/lib"
 
 cd $WORKINGDIR
 
-drupal_files_list=($(ssh $APPSERVERSMASTER 'bash $HOME/rsyncster/drupalfiles_get.sh'))
+if ! [ $APPSERVERSMASTER eq $(hostname -s) ] ; then
+	echo "$(timestamp)- TASK - Getting drupal files list on remote machine $APPSERVERSMASTER"
+	cmd=(ssh $APPSERVERSMASTER 'bash $HOME/rsyncster/drupalfiles_get.sh')
+else
+	echo "$(timestamp)- TASK - Getting local drupal files list on local machine $APPSERVERSMASTER"
+	cmd=('bash $HOME/rsyncster/drupalfiles_get.sh')
+fi
+
+drupal_files_list=$cmd
 
 #mapfile -t <$HOME/rsyncster/virt_domains.list
 
    for d in "${drupal_files_list[@]}"; do
-	echo "$(timestamp) - TASK - Setting up end of year refresh for ${d%%/}" >> $status
-	mkdir -v ${d%%/} >> $status
+	echo "$(timestamp) - TASK - Setting up end of year refresh for ${d%%/}"
+	mkdir -v ${d%%/}
 	cd ${d%%/}
 	touch $PREFIX.${d%%/}
-	echo " - TASK - Creating flag $PREFIX.${d%%/}" in $WORKINGDIR/${d%%/} >> $status
+	echo " - TASK - Creating flag $PREFIX.${d%%/}" in $WORKINGDIR/${d%%/} 
 	cd $WORKINGDIR
    done
