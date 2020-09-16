e53a7162d4ce0c452cbbe8fb8edb15693ef829d1
Kelley Graham <kelley@durastudio.com>
2020-09-16 10:05:44 -0700

    updated nginx config for sites-available, removed nites-enabled check

--- extras/wget_static_standalone.pl
+++ extras/wget_static_standalone.pl
@@ -1,29 +1,23 @@
 #!/usr/bin/perl
 #This script pulls any site into the directory indicated by cli ARGV[1] .
-#Usage:
-#wget.pl "<URL>" <destination>
-
-#use URI; #for later
+#Usage: wget.pl <URL> <destination>
 
 die("Please provide URL. Usage: URL full-path-to-destination\n") unless ($ARGV[0]);
 die("Please provide destination path. Usage: URL full-path-to-destination\n") unless ($ARGV[1]);
 
 my $scheme = "https://";
-my @domains = $ARGV[0];
+my @domains = "$ARGV[0]";
 my $log_dir = "$ARGV[1]";
-my $status = "$log_dir/$ARGV[0]_puller.status";
 
 
-
-chdir('$ARGV[1]') or die "$!";
+chdir("$ARGV[1]") or die "$!";
 
 foreach (@domains) {
 
 	chomp($_);
 	
 	my $host = "$_";
-	my ($top_level) = $host =~ m/([^.]+\.[^.]+$)/;
-	my $URL = "$scheme"."$load_balancer\.$top_level";
+	my $URL = "$scheme"."$host";
 
 	my $dir = "$_";
 		
@@ -34,14 +28,16 @@ foreach (@domains) {
 	} else {
 
 	       	mkdir($dir) or die "$!";
-		system("echo \" - TASK : Creating $dir \" >> $status");
+		system("echo \" - TASK : Creating $dir \"");
 		chdir($dir) or die "$!";
 
 	}
 		
-	my $msg = " - TASK : Fetching files from $host";
-        system("echo \"$msg\" >> $status");
+	my $msg = "\n======== TASK : Fetching files from $host ========\n";
+        system("echo \"$msg\"");
    	system("wget -nH -mpk --base=$host --exclude-directories=$exclude_list --no-check-certificate --user-agent=\"\" --restrict-file-names=windows -e robots=off $URL");
-	system("ls -lah >> $status");
-	system("cat $status");
+
+	system("echo \"\n======== NOTICE : Begin $dir listing ========\n\"");
+	system("ls -lah");
+	system("echo \"\n======== NOTICE : End $dir listing ========\n\"");
 }
