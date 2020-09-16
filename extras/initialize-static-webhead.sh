27205779dd077f898d6c2fd0303a46aa8da876da
root <kelley@durastudio.com>
2020-09-16 12:37:50 -0700

    removed docs dir (duplicate)

--- extras/initialize-static-webhead.sh
+++ extras/initialize-static-webhead.sh
@@ -9,7 +9,8 @@ LIB_PATH="$HOME/manage/rsyncster/lib"
 
 if [ -d /tmp/.init-webhead.lock ]; then
 
-	echo "FAILURE : rsync lock exists : Perhaps there is a lot of new data to push to front end web servers. Will retry soon." >> $status
+	echo "FAILURE : rsync .init-webhead.lock exists : Perhaps there is a lot of new data to push to front end web servers. Will retry soon." >> $status
+	cat $status
 	exit 1
 fi
 
@@ -19,7 +20,8 @@ if [ $1 ]; then
 else
         
 	echo -e "\n\tERROR: You must include the IP address of the new webhead on the command line when invoking this script.\n"
-        exit
+        cat $status
+	exit 1
 
 fi
 
@@ -27,7 +29,8 @@ mkdir -v /tmp/.init-webhead.lock
 
 if [ $? = "1" ]; then
 
-	echo "$(timestamp) - FAILURE : cannot create lock" >> $status
+	echo "$(timestamp) - FAILURE : cannot create .init.webhead.lock" >> $status
+	cat $status
 	exit 1
 
 else
@@ -38,26 +41,44 @@ fi
 
 echo "$(timestamp) - ===== Initializing static webhead with IP $1 =====" >> $status
 
-if  ! ssh root@$WEBHEAD_IP "test -e /var/www/html/live/"; then
+if  ! ssh root@$WEBHEAD_IP "test -d /var/www/html/live"; then
 
-	ssh root@$WEBHEAD_IP "cd /var/www/html/ && ln -s /home/kelley/static_sites live"
-
+	ssh root@$WEBHEAD_IP "mkdir /var/www/html/live" # todo: update for env.sh variable
 fi
 
-nice -n 20 /usr/bin/rsync -avilzx -e ssh /etc/nginx/sites-available/ root@$WEBHEAD_IP:/etc/nginx/sites-available/
-nice -n 20 /usr/bin/rsync -avilzx -e ssh /etc/nginx/sites-enabled/ root@$WEBHEAD_IP:/etc/nginx/sites-enabled/
-nice -n 20 /usr/bin/rsync -avilzx -e ssh /etc/nginx/snippets/ root@$WEBHEAD_IP:/etc/nginx/snippets/
+if "[ -d "/etc/nginx" ]"; then
+         echo " - NOTICE : Found local linux nginx config dir on $i" >> $status
+         LOCAL_NGINX_PATH="/etc/nginx"
+	 LOCAL_NGINX_CMD="systemctl condreload nginx"
+      else
+         echo " - NOTICE : Found local bsd nginx config dir on $i" >> $status
+         LOCAL_NGINX_PATH="/usr/local/etc/nginx"
+	 LOCAL_NGINX_CMD="service nginx reload"
+      fi
 
-ssh root@$WEBHEAD_IP "systemctl condreload nginx"
-ssh root@$WEBHEAD_IP "systemctl status nginx"
+if ssh root@$i "[ -d "/etc/nginx" ]"; then
+         echo " - NOTICE : Found remote linux nginx config dir on $i" >> $status
+         REMOTE_NGINX_PATH="/etc/nginx"
+	 REMOTE_NGINX_CMD="systemctl condreload nginx"
+      else
+         echo " - NOTICE : Found remote bsd nginx config dir on $i" >> $status
+         REMOTE_NGINX_PATH="/usr/local/etc/nginx"
+	 REMOTE_NGINX_CMD="service nginx reload"
+      fi
+
+nice -n 20 /usr/bin/rsync -avilzx -e ssh $LOCAL_NGINX_PATH/sites-available/ root@$WEBHEAD_IP:$REMOTE_NGINX_PATH/sites-available/
+nice -n 20 /usr/bin/rsync -avilzx -e ssh $LOCAL_NGINX_PATH/sites-enabled/ root@$WEBHEAD_IP:$REMOTE_NGINX_PATH/sites-enabled/
+nice -n 20 /usr/bin/rsync -avilzx -e ssh $LOCAL_NGINX_PATH/snippets/ root@$WEBHEAD_IP:$REMOTE_NGINX_PATH/snippets/
+
+ssh root@$WEBHEAD_IP "$REMOTE_NGINX_CMD"
 
 if [ $? = "1" ]; then
 
-	echo "$(timestamp) - FAILURE : rsync failed. Please refer to the solution documentation " >> $status
+	echo "$(timestamp) - FAILURE : init webhead failed. Please refer to the solution documentation " >> $status
 	exit 1
 
 fi
 
-echo "$(timestamp) - ===== Completed rsync of http docroot on $i =====" >> $status
+echo "$(timestamp) - ===== Completed initi webhead on $i =====" >> $status
 rmdir -v /tmp/.initwebhead.lock
-echo "$(timestamp) - SUCCESS : rsync completed successfully" >> $status
+echo "$(timestamp) - SUCCESS : Init webhead completed successfully" >> $status
