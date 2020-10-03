68317b7fd2f9d31fe3f766edf7bbdc214b3a7500
50ten40 <kelley@50ten40.com>
2020-10-03 13:58:45 -0700

    updates

--- end_of_year_site_updates.sh
+++ end_of_year_site_updates.sh
@@ -6,15 +6,33 @@ LIBPATH="$HOME/rsyncster/lib"
 
 cd $WORKINGDIR
 
-echo "$(timestamp)- TASK - Getting local drupal files list on $APPSERVERSMASTER"
+echo "$(timestamp)- TASK - Getting local drupal multisite list on $APPSERVERSMASTER" >> $status
 
 drupal_files_list=$(bash $HOME/rsyncster/drupalfiles_get.sh)
 
    for d in "${drupal_files_list[@]}"; do
-	echo "$(timestamp) - TASK - Setting up end of year refresh for ${d%%/}"
+	echo "$(timestamp) - TASK - Setting up end of year refresh for drupal multisite - ${d%%/}" >> $status
 	mkdir -v ${d%%/}
 	cd ${d%%/}
 	touch $PREFIX.${d%%/}
-	echo " - TASK - Creating flag $PREFIX.${d%%/}" in $WORKINGDIR/${d%%/} 
+	echo -e " - TASK - Created flag $PREFIX.${d%%/} in $WORKINGDIR/${d%%/}\n" 
 	cd $WORKINGDIR
    done
+
+echo "$(timestamp) - TASK - Getting local drupal dev list on $APPSERVERSMASTER" >> $status
+
+drupal_dev_list=$(bash $HOME/rsyncster/drupalfiles_dev.sh)
+
+   for d in "${drupal_dev_list[@]}"; do
+        echo "$(timestamp) - TASK - Setting up end of year refresh for drupal devsite - ${d%%/}" >> $status
+        mkdir -v ${d%%/}
+        cd ${d%%/}
+        touch $PREFIX.${d%%/}
+        echo -e " - TASK - Created flag $PREFIX.${d%%/} in $WORKINGDIR/${d%%/}\n" 
+        cd $WORKINGDIR
+   done
+
+if [ $DEBUG="yes" ] ; then
+   echo -e "============= end of year debug log =============\n"
+   cat $status
+fi
